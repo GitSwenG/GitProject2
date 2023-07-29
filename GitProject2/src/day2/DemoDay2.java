80ca332a78d7d43f183fa2f78c091633487281b9
vitai <vitai@HappyTrees.hsd1.fl.comcast.net>
2023-07-29 08:34:28 -0700

    Created Class

---
+++ GitProject2/src/day2/DemoDay2.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class DemoDay2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Soup");
+		
+	}
+	
+	public static void add(int a, int b) {
+		System.out.println(a + b);
+		
+	}
+}
